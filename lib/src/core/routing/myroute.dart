4ba1a78f2849c9429ea747f0ad28b2038e3a253c
aya11aly <aya.123.aly.5@gmail.com>
2024-05-02 04:53:45 +0300

    ..

---
+++ lib/src/core/routing/myroute.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../dashboard/view/page/dashboard_page.dart';
+
+
+
+class MyRoute {
+  static List< Route <dynamic>> initialRoutes = [
+
+    MaterialPageRoute(
+        builder: (_) => const DashboardPage()),
+  ];
+
+  static Route<dynamic> onNavigateByName(RouteSettings settings)
+  {
+    switch(settings.name)
+    {
+      case 'dashboard':
+        return MaterialPageRoute(builder:(_) => const DashboardPage() );
+      default:
+        return MaterialPageRoute(builder: (_) => const DashboardPage());
+    }
+
+  }
+}
